820d61fc8e3825a3cfc2f54ba3a6b5e980130293
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-07-08 19:36:22 +0200

    Add project files.

---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersControllers.cs
@@ -0,0 +1,21 @@
+using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Arrange
+        var sut = new UsersController();
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+}
